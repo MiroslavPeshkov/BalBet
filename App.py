b8ad8337de00840280483f3285af9ac9cc0b66ae
Miroslav Peshkov <99148327+MiroslavPeshkov@users.noreply.github.com>
2022-08-27 21:41:21 +0300

    Update App.py

--- App.py
+++ App.py
@@ -6,6 +6,10 @@ import pandas as pd
 import re
 import lxml
 import streamlit as st
+import openpyxl
+from openpyxl import Workbook
+import numpy as np
+
 c = {'ASP.NET_SessionId': 'uspd4czxuxtaiqokiilppwpf',
      '_gat': '1',
      '.AspNet.cpsAuth': 'CfDJ8GaK_vc8ictOkQAmLJSxb4QNGXklQcHQEGVvBsc_rKicZdF-XXkLQH4zUAzKjGKa83S9NZJTiWzKwtsN9G3LZ1W5Mfuqwa6fLisyXbWBK83518JQBYjOuiBMoNkGFUsxvBmJqWggeHyrCBXIU-BR6uxi5oMlZjQcaGFDPoQ1_-H7xZxdcbdmL92Ck62Wi7spacXKvviGoo_qkBzUqVXeMGoKQFKlLh3T-f66kKuZk5FeMOFCaIKdTnCIR4G-IywuxjUzGJPYl_OlnG8B-K00qd7K8nVbuwRnan2gHqnZyiB2xtAia6D4fE8-Ef8Auph5Ka1uFiOmjUbjtuCujUaQdXIxoKveaeeZuEYW49ky6ZpNVpSeoWgPlEXwH99YHf2yXHX9xqjtY1YGRlgyLgF0_hRcrjNSvD3uFb5VlwYS0qZT6anGvF4vN_PZuCZES32x50bm8ZfDebQFZyL3-euLBN3C7H0RzgyzeFqbEnTNCtd-JXpjKJgLbiZqOtQXYDsvJyckhESjlC8xXRwzaDnKWb_4fEJ68vp_v76kVSVLfLh0',
@@ -21,7 +25,8 @@ if but:
     pag = pag.find_all('a')[1].text
     pag = int(pag)
     data = []
-    for num in range(1, 4):  # pag+1
+    for num in range(1, 3):  # pag+1
+        st.write('Work with page number - ', num)
         res = requests.get('https://old.baltbet.ru/BetsTota.aspx?page={num}', cookies=c)
         soup = BeautifulSoup(res.text, 'lxml')
         list_1 = soup.find('table', {'class': 'totalmain'}).find_all('a')
